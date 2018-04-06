f4a05c5610575368c05dd1ab77a2d52904b72077
Ariel Ross <arielross92@gmail.com>
2018-04-06 01:11:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,18 +4,19 @@ function init() {
   const down = keydown;
   let index = 0
   function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which)
-    
-    if (key === alphabet[index]){
-      index++
-      
-      if (index===alphabet.length){
-        alert("Hurray!")
-        
-        index = 0
+      var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
       }
     } else {
-      index = 0
+      index = 0;
     }
-  }
+  }, false);
 }
