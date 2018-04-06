59dc7e66621c101844028461cf5cbf701a1d21ae
Ariel Ross <arielross92@gmail.com>
2018-04-06 01:13:56 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,23 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  const down = keydown;
   let index = 0;
-  function onKeyDownHandler(e) {
-      var index = 0;
-  document.addEventListener('keydown', function (e) {
-    const key = parseInt(e.detail || e.which);
 
+  document.body.addEventListener('keydown', function onKeyDownHandler(e){
+    const key = parseInt(e.detail || e.which)
+    console.log(e.detail)
     if (key === code[index]) {
       index++;
 
-      if (index === code.length - 1) {
-        window.alert("YOU DID IT!");
+      if (index === code.length) {
+        alert('Woop!');
+
         index = 0;
       }
     } else {
       index = 0;
     }
-  }, false);
-}
-}
+  })
+  }
